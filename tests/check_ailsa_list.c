c6236e799d3aabafc85936f1acaa51fda64167d8
Iain M Conochie <iain@ailsatech.net>
2023-11-21 20:07:51 +0000

    Initial files for check_ailsa_list

---
+++ tests/check_ailsa_list.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+#include <check.h>
+#include "../include/ailsa.h"
+
+int
+main(void)
+{
+        int failed = 0;
+
+        return (failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
