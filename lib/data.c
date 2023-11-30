c31acd9a8a82ee3f73c216ff43a8171c02f04cf4
Iain M Conochie <iain@ailsatech.net>
2023-11-30 11:56:54 +0000

    Fixed issues with checks

--- lib/data.c
+++ lib/data.c
@@ -69,7 +69,7 @@ ailsa_list_ins_next(AILSA_LIST *list, AILSA_ELEM *element, void *data)
 	size_t size = sizeof(AILSA_ELEM);
 	AILSA_ELEM *new;
 
-	if (!(element) && list->total != 0 || list->total == SIZE_MAX)
+	if ((!(element) && list->total != 0) || list->total == SIZE_MAX)
 		return -1;
 	new = ailsa_calloc(size, "new in ailsa_list_ins_next");
 	new->data = data;
@@ -97,7 +97,7 @@ ailsa_list_ins_prev(AILSA_LIST *list, AILSA_ELEM *element, void *data)
 	size_t size = sizeof(AILSA_ELEM);
 	AILSA_ELEM *new;
 
-	if (!(element) && list->total != 0 || list->total == SIZE_MAX)
+	if ((!(element) && list->total != 0) || list->total == SIZE_MAX)
 		return -1;
 	new = ailsa_calloc(size, "new in ailsa_list_ins_prev");
 	new->data = data;
@@ -126,7 +126,7 @@ ailsa_list_insert_head(AILSA_LIST *list, void *data)
 	int retval = 0;
 	if (!(list) || !(data))
 		return -1;
-	if (list-total == SIZE_MAX)
+	if (list->total == SIZE_MAX)
 		return -1;
 	if (list->total == 0) {
 		retval = ailsa_list_ins_next(list, NULL, data);
